a419e11aa0fa6fa35019a9a4f711ed15e8df41cc
Chris Otto <chris.otto6@gmail.com>
2020-01-04 21:18:54 -0600

    Updating package to use the class better initializing a new instance should work better with managing state

--- README.md
+++ README.md
@@ -4,12 +4,14 @@ NPM package to format fitnesse wiki.
 
 ## Usage
 
-```npm install --save-dev fitnesse-format```
+`npm install --save-dev fitnesse-format`
 
 Import the package into your JS files
 
-```import * as format from fitnesse-format```
+`import * as wikiformatter from fitnesse-format`
 
 The package only takes in a string so you can read in a whole FitNesse test or just a section of the test as string. Just call what you imported it as in order to trigger the formatting.
 
-```formattedWiki = format(string);```
+`const wiki = new wikiformatter();`
+
+`const formattedWiki = wiki.format(string);`
--- index.test.js
+++ index.test.js
@@ -1,47 +1,48 @@
-const format = require("./index");
+const wikiformatter = require("./index");
+const wiki = new wikiformatter();
 
 describe("WikiFormatter", () => {
   it("should format", () => {
     const input = "|column1  |column2     |\n|1             |!(|\n|3|4|";
     const expected = "|column1|column2|\n|1      |!(|\n|3      |4      |";
 
-    expect(format(input)).toBe(expected);
+    expect(wiki.format(input)).toBe(expected);
   });
   it("should not format due to spaces", () => {
     const input = "|column1|column2|\n\n|1|2|";
     const expected = "|column1|column2|\n\n|1|2|";
 
-    expect(format(input)).toBe(expected);
+    expect(wiki.format(input)).toBe(expected);
   });
   it("should not format due no tables", () => {
     const input = "just a regular string nothing to see here";
     const expected = "just a regular string nothing to see here";
 
-    expect(format(input)).toBe(expected);
+    expect(wiki.format(input)).toBe(expected);
   });
   it("should not format due special character", () => {
     const input = "!(DATE)";
     const expected = "!(DATE)";
 
-    expect(format(input)).toBe(expected);
+    expect(wiki.format(input)).toBe(expected);
   });
   it("should trim excess", () => {
     const input = "|column1|column2|  \n|1|2|  ";
     const expected = "|column1|column2|\n|1      |2      |";
 
-    expect(format(input)).toBe(expected);
+    expect(wiki.format(input)).toBe(expected);
   });
   it("should handle ! cells", () => {
     const input = "!|table|\n|column1|column2|\n|1|2|";
     const expected = "!|table         |\n|column1|column2|\n|1      |2      |";
 
-    expect(format(input)).toBe(expected);
+    expect(wiki.format(input)).toBe(expected);
   });
   it("should excape !-| -!", () => {
     const input = "|column!-|-!|column2|\n|1|2|";
     const expected = "|column!-|-!|column2|\n|1          |2      |";
 
-    expect(format(input)).toBe(expected);
+    expect(wiki.format(input)).toBe(expected);
   });
   it("should excape other characters", () => {
     const input =
@@ -49,6 +50,6 @@ describe("WikiFormatter", () => {
     const expected =
       "!|ThisTableWillNotInterpret|!c any directives           |'''it is treated''' |\n|!1 like a literal         |--and is especially useful--|''for test tables.''|\n|^SinceTheyTend            |* to have WikiWord symbols in them.              |";
 
-    expect(format(input)).toBe(expected);
+    expect(wiki.format(input)).toBe(expected);
   });
 });
